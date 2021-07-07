30f53b91a7063316aec4acb88d9117f1831d62da
USUARIO <iangarcia2011@gmail.com>
2021-07-07 14:57:34 -0300

    feat: CardWork component built and styled

--- src/components/CardWork/index.tsx
+++ src/components/CardWork/index.tsx
@@ -1,6 +1,45 @@
-const CardWork = () => {
+import {GiPositionMarker} from "react-icons/gi";
+import {CgDollar} from "react-icons/cg";
+import {IoIosArrowForward} from "react-icons/io"
+import { Container, CardHeader, CardFooter, Category } from "./style";
+
+
+
+interface CardWorkProps{
+  work:{
+    nameWork: string;
+    img: string;
+    category: string
+  };
+  key: string;
+}
+//
+const CardWork = ({work, key}:CardWorkProps) => {
+  const {nameWork, img, category}=work
   return (
-    <h1>Card da trabalho</h1>
+    
+    <Container key={key} >
+      <div>
+        <CardHeader>
+          <img src={img} />
+          <div>
+            <h2>{nameWork}</h2>
+            <Category >{category}</Category>
+          </div>
+        </CardHeader>
+
+        <CardFooter>
+          <div> <GiPositionMarker/> logal</div>
+          <div> <CgDollar/> valor ofertado</div>
+        </CardFooter>
+      </div>
+      <a href="/works/description" >
+        <IoIosArrowForward/>
+      </a>
+    </Container>
+
+    
+    
   );
 };
 
