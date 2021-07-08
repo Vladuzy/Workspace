e2010de27c5a039102f6b8e95644c077bd618568
Adyson Bruno <adysonbruno@gmail.com>
2021-07-08 10:39:08 -0300

    fix: remove test in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import { useAuth } from "./providers/AuthProvider";
 import Routes from "./routes";
 import GlobalStyle from "./styles/global";
 
 function App() {
-  const {
-    token,
-    handleRegister,
-    handleLogin,
-    getUserLoggedInfo,
-    getInfoFromASpecificUser,
-    addMoreInfoUser,
-  } = useAuth();
-  console.log(token);
   return (
     <>
       <h1>App</h1>
