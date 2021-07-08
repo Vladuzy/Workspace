b4e8e41511dfc742b82ba93ec64d1a7cd1b45ff9
Rodrigo-Ferraz <rodrigoloha@gmail.com>
2021-07-08 18:33:18 -0300

    feat: insert logical register and style at page register

---
+++ src/pages/Register/style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+height: 100vh;
+background-color: #F5F4F4;
+`
+
+export const FormContainer = styled.form`
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+Width: 274px;
+Height: 500px;
+margin: 0 auto;
+`
+
+export const InputContainer = styled.input`
+Width: 274px;
+Height: 56px;
+`
+
+export const FooterContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+`
+
+export const FooterContainerLink = styled.p`
+    color: #7065E4
+`
+
+export const ButtonContainer = styled.button`
+    Width: 274px;
+    Height: 56px;
+    background-color: #7065E4;
+    list-style: none;
+    border-style: none;
+    margin-top: 20px;
+    color: #F5F4F4;
+`
+
+export const SpanFormContainer = styled.span`
+    color: #7065E4;
+    font-size: 13px;
+`
+
+export const SelectContainer = styled.select`
+Width: 274px;
+Height: 56px;
+`
