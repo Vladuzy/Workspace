e400b65a0eb9a79bcf3918159ece9de3921e76c3
Leandro Simões <Leandrosimoes42@gmail.com>
2021-07-08 02:30:37 -0300

    ✨ Feat: input styled and finnished

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import Button from "./components/Button";
-
 function App() {
-  return <Button text='alo' handleClick={() => ''} width='100vw' heigth='70px' borderRadius='10px' backColor='#7065E4' border='none' color='white' fontSize={'1.6rem'}/>;
+  return <h1>App</h1>;
 }
 
 export default App;
