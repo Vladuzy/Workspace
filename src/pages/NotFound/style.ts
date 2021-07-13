2a04875d3a73261180a9f8d84a139836ed2df45c
Leandradz <leticia_andrade94@hotmail.com>
2021-07-13 09:26:14 -0300

    style: including style desktop

--- src/pages/NotFound/style.ts
+++ src/pages/NotFound/style.ts
@@ -5,25 +5,26 @@ export const Container = styled.div`
   text-align: center;
 
   > img {
-    margin-top: 20%;
-    width: 250px;
+    margin-top: 50px;
+    min-width: 250px;
+    width: 30vw;
   }
 `;
 
 export const Content = styled.div`
   width: 100%;
-  height: 55%;
+  height: 50vh;
   position: absolute;
   bottom: 0;
   border-top-left-radius: 50px;
   border-top-right-radius: 50px;
-  box-shadow: 0 20px 80px 10px #393739;
-  background-color: #393739;
-  color: #fef9f1;
+  box-shadow: 0 20px 80px 10px var(--preto-cafe);
+  background-color: var(--preto-cafe);
+  color: var(--branco-fundo-body);
   font-size: 25px;
 
   h2 {
-    color: #7065e4;
+    color: var(--roxo-tema-principal);
     text-transform: uppercase;
     line-height: 1.4em;
     padding: 30px 0;
@@ -32,5 +33,21 @@ export const Content = styled.div`
   > div {
     height: 70%;
     padding: 0 50px;
+
+    > p {
+      font-size: 23px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    height: 50vh;
+    position: initial;
+    bottom: 0;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    box-shadow: initial;
+    background-color: var(--branco-fundo-body);
+    color: var(--preto-cafe);
+    font-size: 25px;
   }
 `;
